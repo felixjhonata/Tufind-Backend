94f708eb4a26762977dbd63aa4134336190e76ee
jameshowell08 <howelljamescong@gmail.com>
2024-05-26 15:08:35 +0700

    ds

--- routes/tutorRouter.go
+++ routes/tutorRouter.go
@@ -11,9 +11,9 @@ func TutorRoutes(incomingRoutes *gin.Engine) {
 	protected := incomingRoutes.Group("/api/protected").Use(middlewares.Authentication())
 	{
 		protected.GET("/tutors", controller.GetTutors())
-		protected.GET("/tutors/:id", controller.GetTutor())
-		protected.POST("/tutors", controller.CreateTutor())
-		protected.PATCH("/tutors/:id", controller.UpdateTutor())
+		protected.GET("/tutor/:id", controller.GetTutor())
+		protected.POST("/tutor", controller.CreateTutor())
+		protected.PATCH("/tutor/:id", controller.UpdateTutor())
 	}
 
 }
